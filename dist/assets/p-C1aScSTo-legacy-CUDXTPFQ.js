0a9a3dd8ebb23a27e2504817f5659e9c8b3ad6f7
Bichu Palaparambil <bichu.po@gmail.com>
2026-07-19 02:57:34 +0530

    test 22

--- dist/assets/p-C1aScSTo-legacy-CUDXTPFQ.js
+++ dist/assets/p-C1aScSTo-legacy-CUDXTPFQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3QjLr9my.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.B}],execute:function(){
+System.register(["./index-legacy-kB7WYiES.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.B}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
