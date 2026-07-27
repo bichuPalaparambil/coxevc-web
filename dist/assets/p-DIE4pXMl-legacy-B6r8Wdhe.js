a660c3544942fa16960a0ea6dd2abebf099dc515
Bichu Palaparambil <bichu.po@gmail.com>
2026-07-27 20:06:01 +0530

    slug changed to coxevac

--- dist/assets/p-DIE4pXMl-legacy-B6r8Wdhe.js
+++ dist/assets/p-DIE4pXMl-legacy-B6r8Wdhe.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHi9UP2r.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.r}],execute:function(){
+System.register(["./index-legacy-DGDHHcJc.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.r}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
