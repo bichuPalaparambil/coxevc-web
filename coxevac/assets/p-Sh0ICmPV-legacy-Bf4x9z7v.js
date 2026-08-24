51f2d34cd803da3c9778d0c9a9cf80d5faf62019
Bichu Palaparambil <bichu.po@gmail.com>
2026-08-24 15:50:12 +0530

    coxevac service updates and new backend integration

--- coxevac/assets/p-Sh0ICmPV-legacy-Bf4x9z7v.js
+++ coxevac/assets/p-Sh0ICmPV-legacy-Bf4x9z7v.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C4TJzZEk.js"],function(t,e){"use strict";var n,r,s;return{setters:[t=>{n=t.o,r=t.s,s=t.b}],execute:function(){
+System.register(["./index-legacy-CeKMM-ZV.js"],function(t,e){"use strict";var n,r,s;return{setters:[t=>{n=t.o,r=t.s,s=t.b}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
