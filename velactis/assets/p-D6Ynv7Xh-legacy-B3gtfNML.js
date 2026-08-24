c355983775f8a969089032ee5a84efbf27bab382
Bichu Palaparambil <bichu.po@gmail.com>
2026-08-24 15:30:02 +0530

    bankend url to dev test

--- velactis/assets/p-D6Ynv7Xh-legacy-B3gtfNML.js
+++ velactis/assets/p-D6Ynv7Xh-legacy-B3gtfNML.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CuJjIZaH.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.t}],execute:function(){
+System.register(["./index-legacy-DFyJHHfR.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.t}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
